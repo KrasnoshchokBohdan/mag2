b43b8d2f276a629fa0a69efbb8069fd1c0d7fd3d
DanK894 <82168352+DanK894@users.noreply.github.com>
2022-01-07 18:36:16 +0200

    add files

--- app/code/Order/Cancel/view/frontend/web/js/order-modal.js
+++ app/code/Order/Cancel/view/frontend/web/js/order-modal.js
@@ -7,25 +7,59 @@ define([
         var OrderModal = {
             initModal: function (config, element) {
 
+                var ajax = {
+                    ajaxPostSend: function (obj) {
+                        var url = urlBuilder.build("ordercancel/order/cancel");
+                        $.ajax({
+                            showLoader: true,
+                            url: url,
+                            type: "POST",
+                            dataType: 'json',
+                            context: this
+
+                        }).done(function (respond) {
+                            if (respond.success === true) {
+                                console.log('Done!');
+                            }
+                        });
+                    }
+                };
+
                 var options = {
                     type: 'popup',
                     responsive: true,
                     innerScroll: true,
                     title: 'Cancel Order',
                     modalLeftMargin: 55,
-                    buttons: [{
-                        text: $.mage.__('Close'),
-                        class: 'modal-close',
-                        click: function () {
-                            this.closeModal();
+                    buttons: [
+                        {
+                            text: $.mage.__('Close'),
+                            class: 'modal-close',
+                            click: function () {
+                                console.log("close");
+                                this.closeModal();
+                            }
+                        },
+                        {
+                            text: $.mage.__('Ok'),
+                            class: 'modal-close',
+                            click: function () {
+                                console.log("ok");
+                                ajax.ajaxPostSend(this);
+                                this.closeModal();
+                            }
+
                         }
-                    }]
+
+                    ]
                 };
 
 
                 $target = $(config.target);
                 $target.modal(options);
                 $element = $(element);
+                $orderId = $element.attr('id');
+
                 $element.click(function () {
                     $target.modal('openModal');
                 });
@@ -35,41 +69,42 @@ define([
             'order-modal': OrderModal.initModal
         };
 
-        //  modal(options, $('#modal-content'));
-        //  $(".modal-btn").on('click', function () {
-        //      $("#modal-content").modal("openModal");
-        //  });
+//  modal(options, $('#modal-content'));
+//  $(".modal-btn").on('click', function () {
+//      $("#modal-content").modal("openModal");
+//  });
 
 
-        // return {
-        //     var urlTest = urlBuilder.build("ordercancel/order/cancel");
-        //
-        //     exampleData:function(urlTest){
-        //         $(".modal-btn").click(function(e) {
-        //             e.preventDefault();
-        //             //     var $form = $('#contact-form2');     //what???????
-        //             //     var data = $('#contact-form2').serialize();   //what???????
-        //             //    if(!$form.valid()) return false;
-        //             $.ajax({
-        //                 url:urlTest,
-        //                 type:'POST',
-        //                 showLoader: true,
-        //                 dataType:'json',
-        //                 data: data,
-        //                 complete: function(data) {
-        //                     // $('#email_address').val("");   //what???????
-        //                     modal(options, $('#modal-content')).modal('openModal');
-        //                 },
-        //                 error: function (xhr, status, errorThrown) {
-        //                     console.log('Error happens. Try again.');
-        //                 }
-        //             });
-        //         });
-        //   }
-        //    }
+// return {
+
+//
+//     exampleData:function(urlTest){
+//         $(".modal-btn").click(function(e) {
+//             e.preventDefault();
+//             //     var $form = $('#contact-form2');     //what???????
+//             //     var data = $('#contact-form2').serialize();   //what???????
+//             //    if(!$form.valid()) return false;
+//             $.ajax({
+//                 url:urlTest,
+//                 type:'POST',
+//                 showLoader: true,
+//                 dataType:'json',
+//                 data: data,
+//                 complete: function(data) {
+//                     // $('#email_address').val("");   //what???????
+//                     modal(options, $('#modal-content')).modal('openModal');
+//                 },
+//                 error: function (xhr, status, errorThrown) {
+//                     console.log('Error happens. Try again.');
+//                 }
+//             });
+//         });
+//   }
+//    }
 
 
     }
-);
+)
+;
 
 
