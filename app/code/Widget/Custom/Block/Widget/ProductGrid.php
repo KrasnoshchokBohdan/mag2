06396ddd658f0742a10313ca2168eb25cb7ec3fc
DanK894 <82168352+DanK894@users.noreply.github.com>
2022-03-07 19:00:30 +0200

    condition works

--- app/code/Widget/Custom/Block/Widget/ProductGrid.php
+++ app/code/Widget/Custom/Block/Widget/ProductGrid.php
@@ -28,18 +28,18 @@ class ProductGrid extends ProductsList
     private $serializer;
 
     public function __construct(
-        SerializerInterface $serializer,
-        Context $context,
-        CollectionFactory $productCollectionFactory,
-        Visibility $catalogProductVisibility,
-        HttpContext $httpContext,
-        SqlBuilder $sqlBuilder,
-        Rule $rule,
-        Conditions $conditionsHelper,
-        array $data = [],
-        Json $json = null,
-        LayoutFactory $layoutFactory = null,
-        EncoderInterface $urlEncoder = null,
+        SerializerInterface         $serializer,
+        Context                     $context,
+        CollectionFactory           $productCollectionFactory,
+        Visibility                  $catalogProductVisibility,
+        HttpContext                 $httpContext,
+        SqlBuilder                  $sqlBuilder,
+        Rule                        $rule,
+        Conditions                  $conditionsHelper,
+        array                       $data = [],
+        Json                        $json = null,
+        LayoutFactory               $layoutFactory = null,
+        EncoderInterface            $urlEncoder = null,
         CategoryRepositoryInterface $categoryRepository = null
     ) {
         $this->serializer = $serializer;
@@ -79,26 +79,22 @@ class ProductGrid extends ProductsList
 
     public function createCollection1()
     {
-        $collection1 = $this->productCollectionFactory->create();
-        $collection1->setVisibility($this->catalogProductVisibility->getVisibleInCatalogIds());
+        $collection2 = $this->productCollectionFactory->create();
+        $collection2->setVisibility($this->catalogProductVisibility->getVisibleInCatalogIds());
 
-        $collection1 = $this->_addProductAttributesAndPrices($collection1)
+        $collection2 = $this->_addProductAttributesAndPrices($collection2)
             ->addStoreFilter()
             ->setPageSize($this->getPageSize())
             ->setCurPage($this->getRequest()->getParam($this->getData('page_var_name'), 1))
             ->setOrder($this->getSortBy(), $this->getSortOrder());
 
-        $conditions1 = $this->getConditions();
-        $conditions2 = $this->serializer->unserialize($this->getData('condition2'));
+        $conditions2 = $this->getConditions();
+        $conditions33 = $this->serializer->unserialize($this->getData('condition2'));
 
+        $conditions2->getData('conditions')[0]->setData($conditions33['conditions'][0]);
+        $this->sqlBuilder->attachConditionToCollection($collection2, $conditions2);
 
-        $conditions1->getData('conditions')[0]->getData();
-        $conditions1->getData('conditions')[0]=$conditions2[conditions][0];
-        $conditions2['conditions'][0];
-
-        $this->sqlBuilder->attachConditionToCollection($collection1, $conditions2);
-
-        return $collection1;
+        return $collection2;
     }
 
     public function getSortBy()
